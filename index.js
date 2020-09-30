ff2d8d1be00c4906b43f5c1cd6e14c7825f81b2c
Rob van der Leek <robvanderleek@gmail.com>
2020-09-30 15:41:05 +0200

    Add list command

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 function usage() {
     console.log(
         'Backpack usage:\n' +
+        '  List files in backpack: bp -l\n' +
         '  Put in backpack: bp -i <filename>\n' +
         '  Get from backpack: bp -e <filename>\n')
 }
@@ -29,12 +30,30 @@ function exportFile(filename, backpackDir) {
     fs.renameSync(path.join(backpackDir, filename), filename);
 }
 
+function getStoredFiles(backpackDir) {
+    const files = fs.readdirSync(backpackDir);
+    files.sort((a, b) => fs.statSync(path.join(backpackDir, b)).ctime.getTime() -
+        fs.statSync(path.join(backpackDir, a)).ctime.getTime()
+    );
+    return files;
+}
+
+function listFiles(backpackDir) {
+    const files = getStoredFiles(backpackDir);
+    files.forEach((f, i) => {
+        const index = files.length - i;
+        console.log(`${index}: ${f}`);
+    });
+}
+
 const backpackDir = initializeBackpackDir();
 const args = process.argv.slice(2);
 if (args.length === 2 && args[0] === '-i') {
     importFile(args[1], backpackDir);
 } else if (args.length === 2 && args[0] === '-e') {
     exportFile(args[1], backpackDir);
+} else if (args.length === 1 && args[0] === '-l') {
+    listFiles(backpackDir);
 } else {
     usage();
 }
